383a6af3eb0d742fca6a1e766fd0fb6f033a8303
uzairzia <uzair.governorsindh@gmail.com>
2024-03-01 12:10:25 +0500

    initial commit

---
+++ 9_FavouriteNumber.js
@@ -0,0 +1,6 @@
+"use strict";
+let favouritenumber = 2;
+// let message: string = `My favourite number is ${favouritenumber}`;
+// console.log(message);
+let message = 'My all time favourite number is ' + favouritenumber;
+console.log(message);
---
+++ 9_FavouriteNumber.ts
@@ -0,0 +1,9 @@
+let favouritenumber: number = 2;
+
+// let message: string = `My favourite number is ${favouritenumber}`;
+
+// console.log(message);
+
+let message: string = 'My all time favourite number is ' + favouritenumber;
+
+console.log(message);
